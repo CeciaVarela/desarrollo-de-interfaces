a7ceb9f872e1943b784f297d8b26459fc203f602
Cecía Varela <cvarelar@fpcoruna.afundacion.org>
2022-09-27 09:26:40 +0200

    Ejercicio 1

---
+++ sprint0python/concurso.py
@@ -0,0 +1,10 @@
+print("¿Cuál es la capital de España?")
+print("a.Madrid")
+print("b.Barcelona")
+print("c.Bilbao")
+respuesta = input('\nTeclea una opcion: ')
+if (respuesta != 'a'):
+     print('Respuesta incorrecta')
+else:
+     print('Respuesta correcta')
+	
