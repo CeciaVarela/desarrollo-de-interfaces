baca0c177c31bfb16fc112740b91330d3c0a073f
Cecía Varela <cvarelar@fpcoruna.afundacion.org>
2022-10-18 09:48:32 +0200

    Ejercicio 6

--- sprint1gtk/catalog/window.py
+++ sprint1gtk/catalog/window.py
@@ -4,6 +4,7 @@ from gi.repository import Gtk, GdkPixbuf
 from cell import Cell
 
 class MainWindow(Gtk.Window):
+	flowbox = Gtk.FlowBox()
 	
 	def __init__(self):
 		super().__init__(title="Catálogo")
@@ -11,42 +12,52 @@ class MainWindow(Gtk.Window):
 		self.set_border_width(15)
 		self.set_default_size(400, 400)
 		
-		#Definir el títlo de la página
+		#Definir el título de la página
 		header = Gtk.HeaderBar(title="Animales")
 		header.set_subtitle("Catálogo 2022")
 		header.props.show_close_button = True
 		
 		self.set_titlebar(header)
 		
-		image1 = Gtk.Image()
-		image2 = Gtk.Image()
-		image3 = Gtk.Image()
-		image4 = Gtk.Image()
-		image5 = Gtk.Image()
-		
 		scrolled = Gtk.ScrolledWindow()
 		scrolled.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-		scrolled.add(self.image1)
-		scrolled.add(self.image2)
-		scrolled.add(self.image3)
-		scrolled.add(self.image4)
-		scrolled.add(self.image5)
+		scrolled.add(self.flowbox)
 		self.add(scrolled)
 	
 
 		#Añadir imágenes
-		pixbuf1 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Imagen1.jpg", 200, 200, False)
-		pixbuf2 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Imagen2.jpg", 200, 200, False)
-		pixbuf3 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Imagen3.jpg", 200, 200, False)
-		pixbuf4 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Imagen4.jpg", 200, 200, False)
-		pixbuf5 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Imagen5.jpg", 200, 200, False)
-		self.image1.set_from_pixbuf(pixbuf1)
-		self.image2.set_from_pixbuf(pixbuf2)
-		self.image3.set_from_pixbuf(pixbuf3)
-		self.image4.set_from_pixbuf(pixbuf4)
-		self.image5.set_from_pixbuf(pixbuf5)
+		image = Gtk.Image()
+		pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Imagen1.jpg", 200, 200, False)
+		image.set_from_pixbuf(pixbuf)
 		
-				
+		image2 = Gtk.Image()
+		pixbuf2 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Imagen2.jpg", 200, 200, False)
+		image2.set_from_pixbuf(pixbuf2)
+		
+		image3 = Gtk.Image()
+		pixbuf3 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Imagen3.jpg", 200, 200, False)
+		image3.set_from_pixbuf(pixbuf3)
+		
+		image4 = Gtk.Image()		
+		pixbuf4 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Imagen4.jpg", 200, 200, False)
+		image4.set_from_pixbuf(pixbuf4)
+		
+		image5 = Gtk.Image()
+		pixbuf5 = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Imagen5.jpg", 200, 200, False)
+		image5.set_from_pixbuf(pixbuf5)
+		
+		cell_one = Cell("Mapaches", image)
+		cell_two = Cell("Ardilla", image2)
+		cell_three = Cell("Mariquita", image3)
+		cell_four = Cell("Perro", image4)
+		cell_five = Cell("Hamster", image5)
+		self.flowbox.add(cell_one)
+		self.flowbox.add(cell_two)
+		self.flowbox.add(cell_three)
+		self.flowbox.add(cell_four)
+		self.flowbox.add(cell_five)
+		
+		
 		
 		
 		
