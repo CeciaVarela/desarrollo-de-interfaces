9fc5991cea3bddc2b075ee1bccdea254d2951ad0
Cecía Varela <cvarelar@fpcoruna.afundacion.org>
2022-11-09 09:34:21 +0100

    Ejercicio 3

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -2,14 +2,8 @@ package com.example.mycatalog;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.drawable.RoundedBitmapDrawable;
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
 
