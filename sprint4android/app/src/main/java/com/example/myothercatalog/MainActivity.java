9dffb4626e34d18237fd87b90c93beba7278cc8c
Cecía Varela <cvarelar@fpcoruna.afundacion.org>
2022-11-16 10:03:30 +0100

    Ejercicio 1

--- sprint4android/app/src/main/java/com/example/myothercatalog/MainActivity.java
+++ sprint4android/app/src/main/java/com/example/myothercatalog/MainActivity.java
@@ -1,8 +1,25 @@
 package com.example.myothercatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +27,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        Activity activity = this;
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                "https://github.com/CeciaVarela/desarrollo-de-interfaces/blob/main/API-REST/catalog.json",
+                null,
+                new Response.Listener<JSONArray>(){
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        
+                    }
+                });
+        RequestQueue cola = Volley.newRequestQueue(this);
+        cola.add(request);
     }
 }
